80332dfab2678564faecdda874c52626c565432a
Garnedyna Aulia Rahma <garnedyna.ar@gmail.com>
2022-01-12 20:25:59 +0700

    add login

---
+++ src/pages/VerificationEmail.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import imgVerifemail from '../assets/img/verifemail';
+
+function VerificationEmail() {
+    return (
+      <>
+        <img className="w-75 img-fluid" src={imgVerifemail} alt=""></img>
+        <b>Check Your Emai
+
+
+
+
+
+
+
+
+          
+        </b>
+      </>
+    );
+}
+
+export default VerificationEmail
