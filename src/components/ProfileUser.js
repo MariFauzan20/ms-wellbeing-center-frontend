ee8ec7e34efe5f38950724d24678e53bb3d0d97f
Garnedyna Aulia Rahma <garnedyna.ar@gmail.com>
2022-01-14 00:35:05 +0700

    update login

---
+++ src/components/ProfileUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class ProfileUser extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Profile</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileUser;
